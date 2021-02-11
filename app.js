6e5bd5a73aab6dd87eee3e7a9f9607f670376a9b
Jito Chadha <jitochadha@gmail.com>
2021-02-10 18:57:05 -0800

    npm inquirer

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-
-
-
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
 
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
+//   console.log('complete')
+// });
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if(err) throw new Error(err);
-  console.log('complete')
-});
+inquirer
+  .prompt([
+    {
+      type:'input',
+      name: 'name',
+      message: 'what your name'
+    }
+  ])
+  .then(answers=>console.log(answers));
+
+
+
+console.log(inquirer)
